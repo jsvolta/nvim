e3e5fe7fff592421641b2e1c19c074541ed56970
Yiska <jaden.christie@outlook.com>
2024-10-26 20:24:36 -0300

    Fix undefined global 'vim'

--- lua/yiska/plugins/lsp.lua
+++ lua/yiska/plugins/lsp.lua
@@ -176,6 +176,7 @@ return {
                         },
                         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
                         -- diagnostics = { disable = { 'missing-fields' } },
+                        diagnostics = { globals = {'vim'} }
                     },
                 },
             },
