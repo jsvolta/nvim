a0cbe83ca1d3d7fdcea879bf8d9c969c1db4a6cf
Yiska <jaden.christie@outlook.com>
2024-10-26 18:34:23 -0300

    Add lua/yiska/options.lua

---
+++ lua/yiska/options.lua
@@ -0,0 +1,42 @@
+-- [[ Behavior ]]
+vim.opt.backup = false
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+-- vim.opt.scroll
+vim.opt.smartcase = true
+-- vim.opt.spell
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.tagcase = 'followscs'
+-- vim.opt.timeoutlen
+vim.opt.updatetime = 250
+vim.opt.visualbell = true
+vim.opt.wrapscan = false
+
+-- [[ Format ]] 
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.colorcolumn = {80}
+-- vim.opt.copyindent
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+-- vim.opt.smartindent
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+-- [[ Visual ]]
+
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.number = true
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = 'yes' 
+vim.opt.wrap = false
+
+-- [[ Misc ]]
+-- vim.opt.undodir
